2c9046260ea521ecf0ed49d0712ad362265eb6ba
Denis Dreval <dreval@narod.ru>
2025-04-08 18:03:39 +0300

    improved

--- uploader.go
+++ uploader.go
@@ -46,6 +46,8 @@ type Shot struct {
 	Mime string
 }
 
+
+
 func GetAlbums() []Album {
 	resp, err := http.Get(BaseUrlDev + "/albums")
 	if err != nil {
@@ -109,7 +111,6 @@ func getDateTimeOriginal(imageBytes []byte) (*time.Time, error) {
 	return &tm, nil
 }
 
-
 func PostShot(shot Shot) {
 	shotJson, _ := json.Marshal(shot)
 	reader := bytes.NewReader(shotJson)
@@ -120,70 +121,108 @@ func PostShot(shot Shot) {
 }
 
 func main() {
-	
+
+	fmt.Print("Starting...")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: uploader <directory> [--by-date]")
+		return
+	}
+
 	dirname := os.Args[1]
+	byDate := len(os.Args) > 2 && os.Args[2] == "--by-date"
 
 	dirs, err := os.ReadDir(dirname)
-    if err != nil {
-        fmt.Print(err)
-    }
+	if err != nil {
+		fmt.Println("Error reading root directory:", err)
+		return
+	}
 
-    for _, dir := range dirs {
+	albums := make(map[string]Album)
 
-		if (dir.IsDir()) {
-			album := Album {
-				Name: dir.Name(),
-				UserId: 1,
-				PreviewId: 0,
-			}
 
-			stored_album := PostAlbum(album)	
-			albumdirname := dirname + "\\" + dir.Name()
-			files, err := os.ReadDir(albumdirname)
-			if err != nil {
-				fmt.Print(err)
-			}
-
-			dateStart := "1874-07-24"
-			dateEnd := "1874-07-24"
-
-			parts := strings.Split(album.Name, "_")
-			 
-			if (len(parts) > 1 && len([]rune(parts[1])) == 4) {
-				if num, err := strconv.Atoi(parts[1]); err == nil {
-						dateStart = fmt.Sprintf("%d%s", num, "-01-01")
-						dateEnd = fmt.Sprintf("%d%s", num, "-12-31")
-					} else {
-					decade := parts[1][0:3]
-					fmt.Printf("\n%s\n", decade)
-					if num, err := strconv.Atoi(decade); err == nil {
-						dateStart = fmt.Sprintf("%d%s", num, "0-01-01")
-						dateEnd = fmt.Sprintf("%d%s", num, "9-12-31")						
-						fmt.Printf("%d\n", num)
-					}
-				}
-			}
-
-			for _, file := range files {
-				ext := filepath.Ext(file.Name())
-				filename := albumdirname + "\\" + file.Name()
-				fmt.Printf("%s\n", filename)
-				bytes, _ := os.ReadFile(filename)
-				shot := Shot {
-					AlbumId: stored_album.AlbumId,
-					Name: file.Name(),
-					DateStart: dateStart,
-					DateEnd: dateEnd,
-					Data: bytes,
-					Mime: mime.TypeByExtension(ext),
-					UserId: 1,
-				}
-				PostShot(shot)
-			}
-
-			fmt.Printf("%d\n", stored_album.AlbumId)
-
+	for _, dir := range dirs {
+		if !dir.IsDir() {
+			continue
 		}
-    }
+		albumName := dir.Name()
+		albumDir := filepath.Join(dirname, albumName)
+		files, err := os.ReadDir(albumDir)
+		if err != nil {
+			fmt.Printf("Error reading album folder %s: %v\n", albumName, err)
+			continue
+		}
 
+		// Default date range from album name
+		dateStart := "1874-07-24"
+		dateEnd := "1874-07-24"
+		parts := strings.Split(albumName, "_")
+		if len(parts) > 1 && len(parts[1]) == 4 {
+			if num, err := strconv.Atoi(parts[1]); err == nil {
+				dateStart = fmt.Sprintf("%d-01-01", num)
+				dateEnd = fmt.Sprintf("%d-12-31", num)
+			}
+		} else if len(parts) > 1 && len(parts[1]) >= 3 {
+			decade := parts[1][:3]
+			if num, err := strconv.Atoi(decade); err == nil {
+				dateStart = fmt.Sprintf("%d0-01-01", num)
+				dateEnd = fmt.Sprintf("%d9-12-31", num)
+			}
+		}
+
+		// Pre-create album by default (in no --by-date mode)
+		defaultAlbum := Album{Name: albumName, UserId: 1, PreviewId: 0}
+		defaultAlbum = PostAlbum(defaultAlbum)
+		albums[albumName] = defaultAlbum
+
+		for _, file := range files {
+
+			if file.IsDir() {
+				continue
+			}
+
+			filename := filepath.Join(albumDir, file.Name())
+			fmt.Println("Processing:", filename)
+			data, err := os.ReadFile(filename)
+			if err != nil {
+				fmt.Println("Error reading file:", err)
+				continue
+			}
+
+			ext := filepath.Ext(file.Name())
+			var targetAlbum Album
+
+			if byDate {
+				fmt.Print("Upload by date")
+				if t, err := getDateTimeOriginal(data); err == nil && t != nil {
+					dateKey := fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
+					storedAlbum, exists := albums[dateKey]
+					if !exists {
+						newAlbum := Album{Name: dateKey, UserId: 1, PreviewId: 0}
+						storedAlbum = PostAlbum(newAlbum)
+						albums[dateKey] = storedAlbum
+					}
+					targetAlbum = storedAlbum
+				} else {
+					fmt.Println("Could not extract EXIF date, using fallback album")
+					targetAlbum = defaultAlbum
+				}
+			} else {
+				fmt.Print("Upload by dir")
+				targetAlbum = defaultAlbum
+			}
+
+			shot := Shot{
+				AlbumId:  targetAlbum.AlbumId,
+				Name:     file.Name(),
+				UserId:   1,
+				DateStart: dateStart,
+				DateEnd:   dateEnd,
+				Data:     data,
+				Mime:     mime.TypeByExtension(ext),
+			}
+
+			PostShot(shot)
+		}
+	}
 }
