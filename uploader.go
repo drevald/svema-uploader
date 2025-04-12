176db98166a25577170dab369af6ebf7188c56bb
Denis Dreval <dreval@narod.ru>
2025-04-12 19:27:10 +0300

    some fixes

--- uploader.go
+++ uploader.go
@@ -14,13 +14,14 @@ import (
     "github.com/rwcarlsen/goexif/exif"
     "github.com/rwcarlsen/goexif/mknote"	
 	"time"
+	"syscall"
 )
 
 const (
-	//BaseUrlDev = "http://dobby:7777/api"
-	BaseUrlDev = "http://localhost:8888/api"
-	//BaseUrlDev = "http://svema.valdr.ru/api"
-	//BaseUrlDev = "http://192.168.0.148:7777/api"
+	// BaseUrlDev = "http://dobby:7777/api"
+	BaseUrlDev = "http://localhost:7777/api"
+	// BaseUrlDev = "http://svema.valdr.ru/api"
+	// BaseUrlDev = "http://192.168.0.148:7777/api"
 )
 
 // type errorResponse struct {
@@ -44,9 +45,8 @@ type Shot struct {
 	DateEnd string
 	Data []byte
 	Mime string
+	OrigPath string
 }
-
-
 
 func GetAlbums() []Album {
 	resp, err := http.Get(BaseUrlDev + "/albums")
@@ -65,6 +65,7 @@ func GetAlbums() []Album {
 }
 
 func PostAlbum(album Album) Album {
+	fmt.Printf("...Creating album %s\n", album.Name)
 	albumJson, _ := json.Marshal(album)
 	reader := bytes.NewReader(albumJson)
 	resp, err := http.Post(BaseUrlDev + "/albums", "application/json", reader)
@@ -78,7 +79,8 @@ func PostAlbum(album Album) Album {
 	return updated_album
 }
 
-func getDateTimeOriginal(imageBytes []byte) (*time.Time, error) {
+
+func getExifDate(imageBytes []byte) (*time.Time, error) {
 	// Register manufacturer-specific notes
 	exif.RegisterParsers(mknote.All...)
 
@@ -111,6 +113,56 @@ func getDateTimeOriginal(imageBytes []byte) (*time.Time, error) {
 	return &tm, nil
 }
 
+func getLastModifiedDate(path string) (*time.Time, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		return nil, err
+	}
+	modTime := info.ModTime()
+	return &modTime, nil
+}
+
+func getFileCreationDate(path string) (*time.Time, error) {
+	p, err := syscall.UTF16PtrFromString(path)
+	if err != nil {
+		return nil, err
+	}
+	h, err := syscall.CreateFile(p, syscall.GENERIC_READ, syscall.FILE_SHARE_READ, nil,
+		syscall.OPEN_EXISTING, syscall.FILE_ATTRIBUTE_NORMAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	defer syscall.CloseHandle(h)
+
+	var data syscall.ByHandleFileInformation
+	err = syscall.GetFileInformationByHandle(h, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	ft := data.CreationTime
+	t := time.Unix(0, ft.Nanoseconds())
+	return &t, nil
+}
+
+func getShotCreationDate(path string) (*time.Time, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Printf("Fail to read %s\n", path)
+		return nil, err
+	} 
+	t, err := getExifDate(data)
+	if err != nil {
+		fmt.Printf("Failed to get EXIF from %s\n", path)
+		t, err = getLastModifiedDate(path)
+		if err != nil {
+			return nil, err
+		} 
+		return t, nil
+	}
+	return t, nil
+}
+
 func PostShot(shot Shot) {
 	shotJson, _ := json.Marshal(shot)
 	reader := bytes.NewReader(shotJson)
@@ -119,6 +171,8 @@ func PostShot(shot Shot) {
 		fmt.Println(err)
 	}
 }
+
+///////////////////////////////////////////////
 
 func main() {
 
@@ -140,11 +194,35 @@ func main() {
 
 	albums := make(map[string]Album)
 
+	filesTotal := 0
+
+	for _, entry := range dirs {
+		fmt.Println(entry.Name()) // safer than fmt.Print(entry)
+	
+		if entry.IsDir() {
+			subdirPath := filepath.Join(dirname, entry.Name()) // full path to subdirectory
+			files, err := os.ReadDir(subdirPath)
+			if err != nil {
+				fmt.Println("Error reading subdir:", err)
+				continue
+			}
+	
+			for _, file := range files {
+				if !file.IsDir() {
+					filesTotal++
+				}
+			}
+		}
+	}
+
+	filesSent := 0
 
 	for _, dir := range dirs {
+
 		if !dir.IsDir() {
 			continue
 		}
+
 		albumName := dir.Name()
 		albumDir := filepath.Join(dirname, albumName)
 		files, err := os.ReadDir(albumDir)
@@ -159,21 +237,23 @@ func main() {
 		parts := strings.Split(albumName, "_")
 		if len(parts) > 1 && len(parts[1]) == 4 {
 			if num, err := strconv.Atoi(parts[1]); err == nil {
-				dateStart = fmt.Sprintf("%d-01-01", num)
-				dateEnd = fmt.Sprintf("%d-12-31", num)
+				dateStart = fmt.Sprintf("%d-01-01T00:00:00 +0000 UTC", num)
+				dateEnd = fmt.Sprintf("%d-12-31T00:00:00 +0000 UTC", num)
 			}
 		} else if len(parts) > 1 && len(parts[1]) >= 3 {
 			decade := parts[1][:3]
 			if num, err := strconv.Atoi(decade); err == nil {
-				dateStart = fmt.Sprintf("%d0-01-01", num)
-				dateEnd = fmt.Sprintf("%d9-12-31", num)
+				dateStart = fmt.Sprintf("%d0-01-01T00:00:00 +0000 UTC", num)
+				dateEnd = fmt.Sprintf("%d9-12-31T00:00:00 +0000 UTC", num)
 			}
 		}
 
 		// Pre-create album by default (in no --by-date mode)
 		defaultAlbum := Album{Name: albumName, UserId: 1, PreviewId: 0}
-		defaultAlbum = PostAlbum(defaultAlbum)
-		albums[albumName] = defaultAlbum
+		if (!byDate) {
+			defaultAlbum = PostAlbum(defaultAlbum)
+			albums[albumName] = defaultAlbum
+		}		
 
 		for _, file := range files {
 
@@ -193,8 +273,10 @@ func main() {
 			var targetAlbum Album
 
 			if byDate {
-				fmt.Print("Upload by date")
-				if t, err := getDateTimeOriginal(data); err == nil && t != nil {
+				fmt.Println("Upload by date")
+				if t, err := getShotCreationDate(filename); err == nil && t != nil {
+					dateStart = t.Format(time.RFC3339) 
+					dateEnd = t.Format(time.RFC3339) 
 					dateKey := fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
 					storedAlbum, exists := albums[dateKey]
 					if !exists {
@@ -220,9 +302,12 @@ func main() {
 				DateEnd:   dateEnd,
 				Data:     data,
 				Mime:     mime.TypeByExtension(ext),
+				OrigPath:	filename,
 			}
 
 			PostShot(shot)
+			filesSent++
+			fmt.Printf("%d of %d sent\n", filesSent, filesTotal);
 		}
 	}
 }
