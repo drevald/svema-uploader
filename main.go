5705f2043c1ac8ea1472b6e5eda7b577742e450d
Denis Dreval <dreval@narod.ru>
2026-03-07 18:54:16 +0300

    Icon added

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	_ "embed"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 )
 
+//go:embed design/icon.svg
+var iconData []byte
+
 func main() {
 	myApp := app.New()
+	icon := fyne.NewStaticResource("icon.svg", iconData)
+	myApp.SetIcon(icon)
+
 	myWindow := myApp.NewWindow("Svema Photo Uploader")
+	myWindow.SetIcon(icon)
 	myWindow.Resize(fyne.NewSize(800, 600))
 
 	showLoginScreen(myWindow, myApp)
